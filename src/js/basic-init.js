576997bc3c82aa741b650be6df5122e524f8b1bf
Sergey Konovalenko <sergeykonovalenko5550199@gmail.com>
2020-05-06 11:54:40 +0300

    Сделал параллакс

--- src/js/basic-init.js
+++ src/js/basic-init.js
@@ -1,6 +1,25 @@
 $(document).ready(function () {
     'use strict';
 
-    console.log('Welcome');
+    const element = document.documentElement;
+
+    // is mobile
+    const is_mobile = isMobile();
+    if (is_mobile) {
+        element.classList.add('is-mobile');
+    }
+
+    // run parallax
+    if (!is_mobile) {
+        let scene = document.querySelectorAll('.scene');
+        scene.forEach(function (sceneItem) {
+            let parallaxInstance = new Parallax(sceneItem);
+        });
+    }
+
+    // is mobile
+    function isMobile() {
+        return $.browser.device = (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()));
+    }
 
 }); // end ready
--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,8 @@
-import '@fancyapps/fancybox';
+import '@fancyapps/fancybox/dist/jquery.fancybox.min';
 import './vendor/jquery.mb.browser';
+// import 'parallax-js';
+// require('parallax-js');
+
 import './svg-sprite';
 import './basic-init';
 
