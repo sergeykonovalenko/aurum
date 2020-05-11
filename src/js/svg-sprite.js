3c58cbc204860215833f0f60dcb09f9f951ec5e0
Sergey Konovalenko <sergeykonovalenko5550199@gmail.com>
2020-05-11 11:47:14 +0300

    Сделал блок -Остались вопросы?-

--- src/js/svg-sprite.js
+++ src/js/svg-sprite.js
@@ -1,3 +1,4 @@
 import '../img/svg-sprite/icon-shopping-cart.svg';
 import '../img/svg-sprite/icon-logo.svg';
 import '../img/svg-sprite/icon-email.svg';
+import '../img/svg-sprite/icon-question-mark.svg';
