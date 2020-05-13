d8b87bf8e7c60e358a2be5aa0b7759b21dbc650d
Sergey Konovalenko <sergeykonovalenko5550199@gmail.com>
2020-05-13 17:36:16 +0300

    Сделал подвал

--- src/js/svg-sprite.js
+++ src/js/svg-sprite.js
@@ -12,3 +12,5 @@ import '../img/svg-sprite/icon-phone-gradient.svg';
 import '../img/svg-sprite/icon-envelope-gradient.svg';
 import '../img/svg-sprite/icon-marker-gradient.svg';
 import '../img/svg-sprite/icon-clock-gradient.svg';
+import '../img/svg-sprite/icon-instagram.svg';
+import '../img/svg-sprite/icon-facebook.svg';
