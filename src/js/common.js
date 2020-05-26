b90ad6dd46aecfec5581903c57f092d8642b767f
Sergey Konovalenko <sergeykonovalenko5550199@gmail.com>
2020-05-27 00:05:40 +0300

    Добавил страницу 404

--- src/js/common.js
+++ src/js/common.js
@@ -296,68 +296,42 @@ $(document).ready(function () {
             },
             submitHandler: function (form) {
 
-                let submitBtn = $(form).find('button');
-                let submitBtnText = submitBtn.html();
-
                 // get url for redirection
-                let redirect_url = $(form).find('#redirect_url').val();
+                // let redirect_url = $(form).find('#redirect_url').val();
 
                 $.ajax({
-                    url: '/sys-send-request/',
+                    url: `${templateUrl}/tpl-sys-request.php`, // /sys-send-request/
                     data: new FormData(form),
                     type: 'POST',
                     processData: false,
                     contentType: false,
                     beforeSend: function() {
-                        submitBtn.prop('disabled', true).html('Отправка...');
+                        // submitBtn.prop('disabled', true).html('Отправка...');
                     },
                     success: function (data) {
 
                         // enable button
-                        submitBtn.prop('disabled', false).html(submitBtnText);
+                        // submitBtn.prop('disabled', false).html(submitBtnText);
 
-                        if ( data == '1' ) {
+                        // if ( redirect_url ) { // if need redirect
+                        //     window.location.replace( redirect_url );
+                        // }
 
-                            if ( redirect_url ) { // if need redirect
-                                window.location.replace( redirect_url );
+                        $.fancybox.open({
+                            src: '#js-thanks-modal',
+                            type : 'inline',
+                            touch : false,
+                            btnTpl: {
+                                smallBtn: `
+                                        <button class="common-modal__close fancybox-button fancybox-close-small" type="button" data-fancybox-close title="Закрыть">
+                                            <svg width="15" height="15" viewBox="0 0 320 320" fill="#000" xmlns="http://www.w3.org/2000/svg"><path d="M207.6 160L315.3 52.3c6.2-6.2 6.2-16.3 0-22.6l-25-25c-6.2-6.2-16.3-6.2-22.6 0L160 112.4 52.3 4.7c-6.2-6.2-16.3-6.2-22.6 0l-25 25c-6.2 6.2-6.2 16.3 0 22.6L112.4 160 4.7 267.7c-6.2 6.2-6.2 16.3 0 22.6l25 25c6.2 6.2 16.3 6.2 22.6 0L160 207.6l107.7 107.7c6.2 6.2 16.3 6.2 22.6 0l25-25c6.2-6.2 6.2-16.3 0-22.6L207.6 160z"/></svg>
+                                        </button>`
+                            },
+                            afterClose: function () {
+                                $.fancybox.close();
                             }
-
-                            $.fancybox.open({
-                                src: `<div class="modal-thanks modal-common">
-                                          <h4 class="modal-thanks__title">Спасибо, за Ваше обращение. Мы свяжемся с Вами в скором времени</h4>
-                                      </div>`,
-                                type : 'html',
-                                touch : false,
-                                btnTpl: {
-                                    smallBtn: `
-                                        <button class="modal-common__close fancybox-button fancybox-close-small" type="button" data-fancybox-close title="Закрыть">
-                                            <svg width="15" height="15" viewBox="0 0 320 320" fill="#000" xmlns="http://www.w3.org/2000/svg"><path d="M207.6 160L315.3 52.3c6.2-6.2 6.2-16.3 0-22.6l-25-25c-6.2-6.2-16.3-6.2-22.6 0L160 112.4 52.3 4.7c-6.2-6.2-16.3-6.2-22.6 0l-25 25c-6.2 6.2-6.2 16.3 0 22.6L112.4 160 4.7 267.7c-6.2 6.2-6.2 16.3 0 22.6l25 25c6.2 6.2 16.3 6.2 22.6 0L160 207.6l107.7 107.7c6.2 6.2 16.3 6.2 22.6 0l25-25c6.2-6.2 6.2-16.3 0-22.6L207.6 160z"/></svg>
-                                        </button>`
-                                },
-                                afterClose: function () {
-                                    $.fancybox.close();
-                                }
-                            });
-                            jQuery(form).find('.form__field').each(function(){ jQuery(this).val(''); });
-
-                        } else {
-                            $.fancybox.open({
-                                src: `<div class="modal-thanks modal-common">
-                                          <h4 class="modal-thanks__title">Произошла ошибка при отправке заявки. Повторите отправку или перезвоните нам!</h4>
-                                      </div>`,
-                                type : 'html',
-                                touch : false,
-                                btnTpl: {
-                                    smallBtn: `
-                                        <button class="modal-common__close fancybox-button fancybox-close-small" type="button" data-fancybox-close title="Закрыть">
-                                            <svg width="15" height="15" viewBox="0 0 320 320" fill="#000" xmlns="http://www.w3.org/2000/svg"><path d="M207.6 160L315.3 52.3c6.2-6.2 6.2-16.3 0-22.6l-25-25c-6.2-6.2-16.3-6.2-22.6 0L160 112.4 52.3 4.7c-6.2-6.2-16.3-6.2-22.6 0l-25 25c-6.2 6.2-6.2 16.3 0 22.6L112.4 160 4.7 267.7c-6.2 6.2-6.2 16.3 0 22.6l25 25c6.2 6.2 16.3 6.2 22.6 0L160 207.6l107.7 107.7c6.2 6.2 16.3 6.2 22.6 0l25-25c6.2-6.2 6.2-16.3 0-22.6L207.6 160z"/></svg>
-                                        </button>`
-                                },
-                                afterClose: function () {
-                                    $.fancybox.close();
-                                }
-                            });
-                        }
+                        });
+                        // jQuery(form).find('.form__field').each(function(){ jQuery(this).val(''); });
                     }
                 });
 
