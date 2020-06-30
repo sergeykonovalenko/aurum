7c4a9e2a893d75e97beb24c02e3d86350dddbba3
Sergey Konovalenko <sergeykonovalenko5550199@gmail.com>
2020-07-01 00:14:05 +0300

    Добавил акцию

--- src/js/common.js
+++ src/js/common.js
@@ -276,35 +276,42 @@ $(document).ready(function () {
         ]
     });
 
-    // slider product gallery
-    $('.product-gallery').slick({
-        dots: false,
-        arrows: false,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        asNavFor: '.product-gallery-side__wr',
-        pauseOnFocus: false,
-        pauseOnHover: false,
-        // focusOnSelect: true,
-    });
+    // SLIDERS IN THE PRODUCT CARD
+    let productGalleryAll = document.querySelectorAll('.product-gallery');
 
-    // slider product gallery side
-    $('.product-gallery-side__wr').slick({
-        dots: false,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        asNavFor: '.product-gallery',
-        pauseOnFocus: false,
-        pauseOnHover: false,
-        focusOnSelect: true,
-        prevArrow: `<button class="product-gallery-side__arrow product-gallery-side__arrow--prev" type="button">
+    productGalleryAll.forEach(productGallery => {
+        let productGallerySide = productGallery.closest('.product-card').querySelector('.product-gallery-side__wr');
+
+        // slider product gallery
+        $(productGallery).slick({
+            dots: false,
+            arrows: false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            asNavFor: $(productGallerySide),
+            pauseOnFocus: false,
+            pauseOnHover: false,
+            // focusOnSelect: true,
+        });
+
+        // slider product gallery side
+        $(productGallerySide).slick({
+            dots: false,
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            asNavFor: $(productGallery),
+            pauseOnFocus: false,
+            pauseOnHover: false,
+            focusOnSelect: true,
+            prevArrow: `<button class="product-gallery-side__arrow product-gallery-side__arrow--prev" type="button">
                         <span class="visually-hidden">Назад</span>
                         <svg width="14" viewBox="0 0 149 256.1" fill="#999999" xmlns="http://www.w3.org/2000/svg"><path d="M3.5 119.5l117.8-116c4.7-4.7 12.3-4.7 17 0l7.1 7.1c4.7 4.7 4.7 12.3 0 17L43.1 128l102.2 100.4c4.7 4.7 4.7 12.3 0 17l-7.1 7.1c-4.7 4.7-12.3 4.7-17 0L3.4 136.5c-4.6-4.7-4.6-12.3.1-17z"/></svg>
                     </button>`,
-        nextArrow: `<button class="product-gallery-side__arrow product-gallery-side__arrow--next" type="button">
+            nextArrow: `<button class="product-gallery-side__arrow product-gallery-side__arrow--next" type="button">
                         <span class="visually-hidden">Вперед</span>
                         <svg width="14" viewBox="0 0 149 256.1" fill="#999999" xmlns="http://www.w3.org/2000/svg"><path d="M145.4 136.5l-117.8 116c-4.7 4.7-12.3 4.7-17 0l-7.1-7.1c-4.7-4.7-4.7-12.3 0-17L105.8 128 3.6 27.6c-4.7-4.7-4.7-12.3 0-17l7.1-7.1c4.7-4.7 12.3-4.7 17 0l117.8 116c4.6 4.7 4.6 12.3-.1 17z"/></svg>
                     </button>`,
+        });
     });
 
     // transfer of order parameters to the form
@@ -379,10 +386,10 @@ $(document).ready(function () {
         maxFiles: 10, // ограничить максимальное количество файлов
         timeout: 180000,
         acceptedFiles: 'image/jpeg, image/png',
-        dictRemoveFile: 'Видалити',
-        dictCancelUpload: 'Скасувати',
-        dictMaxFilesExceeded: 'Максимальна кількість файлів - {{maxFiles}}',
-        dictFileTooBig: 'Файл занадто великий ({{filesize}}Mb). Макс. розмір файлу – {{maxFilesize}}Mb.',
+        dictRemoveFile: dictRemoveFile,
+        dictCancelUpload: dictCancelUpload,
+        dictMaxFilesExceeded: dictMaxFilesExceeded,
+        dictFileTooBig: dictFileTooBig,
         init: function () {
 
             let form = this.element.closest('form');
@@ -431,7 +438,7 @@ $(document).ready(function () {
 
                 console.log('Событие sendingmultiple');
 
-                feedbackFormButton.textContent = "Завантаження...";
+                feedbackFormButton.textContent = feedbackFormButtonFilesUploaded; // Завантаження...
                 feedbackFormButton.disabled = true;
             });
 
