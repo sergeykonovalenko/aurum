166dfa2ac12fca793d885411b5fa0d88e1506338
Sergey Konovalenko <sergeykonovalenko5550199@gmail.com>
2020-07-01 13:04:18 +0300

    Добавил WOW.js

--- src/js/common.js
+++ src/js/common.js
@@ -1,4 +1,5 @@
 import Dropzone from 'dropzone';
+import {WOW} from 'wowjs';
 import 'jquery-ui/ui/widgets/tabs';
 import 'rater-jquery';
 import '@fancyapps/fancybox/dist/jquery.fancybox.min';
@@ -65,6 +66,23 @@ $(document).ready(function () {
             }
         }
     });
+
+    // init wow
+    let wow = new WOW(
+        {
+            boxClass:     'wow',      // animated element css class (default is wow)
+            animateClass: 'animated', // animation css class (default is animated)
+            offset:       0,          // distance to the element when triggering the animation (default is 0)
+            mobile:       true,       // trigger animations on mobile devices (default is true)
+            live:         false,       // act on asynchronously loaded content (default is true)
+            callback:     function(box) {
+                // the callback is fired every time an animation is started
+                // the argument that is passed in is the DOM node being animated
+            },
+            scrollContainer: null // optional scroll container selector, otherwise use window
+        }
+    );
+    wow.init();
 
     // при клике на карточку товара узнаем имя товара на карточке товара
     let productItems = document.querySelectorAll('.product-item');
